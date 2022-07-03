996df258e296c5d81f3ed562af5bad3000267477
BrayanCahuana <79492288+BrayanCahuana@users.noreply.github.com>
2022-07-03 17:07:22 -0500

    Update p1_propuesto_n°1.py

--- MP-B/p1_propuesto_n°1.py
+++ MP-B/p1_propuesto_n°1.py
@@ -1,8 +1,1 @@
 x = int(input("Ingrese un numero para evaluar: "))
-
-if x > 0:
-    print("El numero es positivo")
-elif x < 0:
-    print("El numero es negativo")
-elif x == 0:
-    print("Es igual a cero")
